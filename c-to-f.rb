e00d12078d39112b08c3a2c2b6066fbdbc7e7b68
janvmusic <djmatt25@gmail.com>
2019-06-02 18:46:03 -0600

    Shorten messages

--- c-to-f.rb
+++ c-to-f.rb
@@ -1,6 +1,2 @@
 print 'Hello. Please enter Celcius value: '
-celcius = gets
-fahrenheit = (celcius.to_i * 9 / 5) + 32
-print 'The result is: '
-print fahrenheit
-puts '.'
+puts "The fahrenheit equivalent is:  #{(gets.to_i * 9 / 5) + 32}"
