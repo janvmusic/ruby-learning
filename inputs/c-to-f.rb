a28a87ece4b310f7b153c48c7d6334daf73df242
jnunez <jnunez@switchfly.com>
2019-06-12 18:46:21 -0600

    Add ticket example

---
+++ inputs/c-to-f.rb
@@ -0,0 +1,2 @@
+print 'Hello. Please enter Celcius value: '
+puts "The fahrenheit equivalent is:  #{(gets.to_i * 9 / 5) + 32}"
---
+++ ticket.rb
@@ -0,0 +1,26 @@
+class Ticket
+
+  def date
+   '1903-01-02'
+  end
+
+  def venue
+    'Town Hall'
+  end
+
+  def event
+    'Author\'s reading'
+  end
+
+  def performer
+    'Mark Twain'
+  end
+
+  def seat
+    'Second Balcony, row J, seat 12'
+  end
+
+  def price
+   5.50
+  end
+end
